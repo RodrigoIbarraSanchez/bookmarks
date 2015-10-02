7424a57244f21f9c240e09b391f7c30ed1798ebf
RodrigoIbarraSanchez <lookatthemoon@live.com.mx>
2015-10-01 23:10:34 -0500

    avance

--- js/app.js
+++ js/app.js
@@ -6,15 +6,25 @@
     ])
     
     .controller('MainController', function($scope){
-        $scope.name = 'John Doe';
+        $scope.name = 'Rodrigo Ibarra';
         $scope.categories = ['HTML5', 'JavaScript', 'CSS', 'Games'];
         $scope.bookmarks = [
-            {id:1,name:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaSript'},
+            {id:1,name:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
             {id:2,name:'HTML5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
             {id:3,name:'CSS Tricks',url:'https://css-tricks.com',category:'CSS'},
             {id:4,name:'Bootstrap',url:'https://getbootstrap.com',category:'CSS'},
             {id:5,name:'Card',url:'https://jessepollak,github.io/card/',category:'JavaSript'},
         ];
+        
+        $scope.currentCategory = 'JavaScript';
+        
+        $scope.setCurrentCategory = function(category){
+            $scope.currentCategory = category;
+        }
+        
+        $scope.isCurrentCategory = function(category){
+            return $scope.currentCategory === category;
+        }
     });
     
 })();
